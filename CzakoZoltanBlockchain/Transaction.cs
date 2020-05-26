e44ad63298d51c3c6c3c8f70eec695c42901c93c
avmesquita <avmesquita@gmail.com>
2020-05-26 22:02:47 +0100

    First commit

---
+++ CzakoZoltanBlockchain/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CzakoZoltanBlockchain
+{
+    /// <summary>
+    /// https://towardsdatascience.com/blockchain-explained-using-c-implementation-fb60f29b9f07
+    /// </summary>
+    public class Transaction
+    {
+        public string From { get; }
+        public string To { get; }
+        public double Amount { get; }
+        public Transaction(string from, string to, double amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
